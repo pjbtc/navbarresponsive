6527de8b5694515cb505d803e6f35bcdc22a6730
pjbtc <pjbhattachan@gmail.com>
2021-05-10 21:42:35 +0100

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div>
+            <h1>About me, I m software engineer and webdeveloper.</h1>
+            
+        </div>
+    )
+}
